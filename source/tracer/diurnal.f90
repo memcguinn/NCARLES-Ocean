c159b61d233ab8be44853209a2c662a7371f3ae6
memcguinn <Mary.Mcguinn@Colorado.EDU>
2022-03-29 10:06:06 -0600

    add diurnal forcing

--- source/tracer/diurnal.f90
+++ source/tracer/diurnal.f90
@@ -4,12 +4,22 @@ SUBROUTINE diurnal
   USE inputs
   USE con_data
 !
-  t_di = time
+  t_les = time
+  day = t_les/86400
   pi     = 4.0*ATAN(1.0)            ! Pi = 3.1417
-  q0_cool = 0.200
-  qd_heat = -1.834
+  pi2    = 2.0*pi                   !
+  d_to_r = pi2/360.0
+
+  IF (day .LE. 1) THEN
+      t_di = t_les
+  ELSE
+      t_di = t_les - (FLOOR(day)*86400)
+  END IF
+
+  q0_cool = .0002
+  qd_heat = .2
   t_heat  = 86400/3                 ! (s)
-  cos_arr = (/(COS(2*pi*t_di/(3600*24))), (COS(pi*t_heat))/)
+  cos_arr = (/(COS((2*pi*t_di/(3600*24))*d_to_r)), (COS((pi*t_heat)*d_to_r))/)
   qstar(1) = q0_cool + qd_heat * (MAXVAL(cos_arr) - COS(pi*t_heat))
   wtsfc(1) = qstar(1)
 !
