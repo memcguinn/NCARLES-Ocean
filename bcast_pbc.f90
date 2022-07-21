a8dda020aef1ed44ecfd80ef4847ea0c029f8db9
memcguinn <Mary.Mcguinn@Colorado.EDU>
2022-07-21 00:19:11 -0600

    ncarles_v2.4.1 --> update boundary files to f90 --> f_suft2

---
+++ bcast_pbc.f90
@@ -0,0 +1,45 @@
+SUBROUTINE bcast_pbc
+! SEND UPPER BC TO OTHER PROCESSORS FOR FFT SOLUTION OF PRESSURE
+
+  USE pars
+  USE fields
+  IMPLICIT NONE
+
+  INCLUDE 'mpif.h'
+  INTEGER :: istatus(mpi_status_size)
+  INTEGER :: irow_r
+
+  IF(numprocs .NE. 1) THEN
+    irow_r = MOD(myid,ncpu_s)
+    irow_t = is_s(numprocs-1) + irow_r
+    num = nnx*(iye+1-iys)
+
+    ! CHECK WHICH ROW MYID IS IN
+    IF(iss .NE. is_s(numprocs-1)) THEN
+      ! NOT IN TOP ROW, RECEIVE FROM TOP
+      CALL mpi_recv(pbc(1,iys,1),num,mpi_real8,irow_t,1,mpi_comm_world,     &
+      istatus,ierr)
+    ELSE
+      ! MYID IS IN THE TOP ROW, SEND BELOW
+      DO l=irow_r,irow_t-ncpu_s,ncpu_s
+        CALL mpi_send(pbc(1,iys,1),num,mpi_real8,l,1,mpi_comm_world,ierr)
+      END DO
+    ENDIF
+
+    ! REPEAT FOR NEXT VARIABLE
+    IF(iss .NE. is_s(numprocs-1)) THEN
+      ! NOT IN TOP ROW, RECEIVE FROM TOP
+      CALL mpi_recv(pbc2(1,iys,1),num,mpi_real8,irow_t,1,mpi_comm_world,    &
+      istatus,ierr)
+    ELSE
+      ! MYID IS IN THE TOP ROW, SEND BELOW
+      DO l=irow_r,irow_t-ncpu_s,ncpu_s
+        CALL mpi_send(pbc2(1,iys,1),num,mpi_real8,l,1,mpi_comm_world,ierr)
+      END DO
+    ENDIF
+  ELSE
+    CONTINUE
+  ENDIF
+
+  RETURN
+END SUBROUTINE
---
+++ source/boundary/f_suft2.f90
@@ -0,0 +1,30 @@
+SUBROUTINE f_suft2(rbuf,nnx,mxs,mxe,iys,iye,nscl,tau13m,tau23m,taut3m,t_grnd)
+
+  IMPLICIT NONE
+
+  ! FILL SURFACE ARRAYS ON ROOT PROCESSORS
+  INTEGER :: iscl , ix , iy , iye , iys , mxe , mxs , nnx , nscl
+  
+  REAL :: rbuf(2+2*nscl,mxs:mxe,iys:iye)
+  REAL ::                                                                   &
+          tau13m(nnx,iys:iye), tau23m(nnx,iys:iye),                         &
+          taut3m(nnx,iys:iye,nscl), t_grnd(nnx,iys:iye,nscl)
+
+  DO iy=iys,iye
+    DO ix=mxs,mxe
+      tau13m(ix,iy) = rbuf(1,ix,iy)
+      tau23m(ix,iy) = rbuf(2,ix,iy)
+    ENDDO
+  ENDDO
+
+  DO iscl=1,nscl
+    DO iy=iys,iye
+      DO ix=mxs,mxe
+        taut3m(ix,iy,iscl) = rbuf(2+iscl,ix,iy)
+        t_grnd(ix,iy,iscl) = rbuf(2+nscl+iscl,ix,iy)
+      ENDDO
+    ENDDO
+  ENDDO
+
+  RETURN
+END SUBROUTINE
